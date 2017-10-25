401bb3a55d32da400fb7188262e55911182dd7b1
johndvnguyen <johndvnguyen@gmail.com>
2017-10-25 16:45:19 -0700

    Update statusTask.c

--- statusTask.c
+++ statusTask.c
@@ -7,10 +7,12 @@
 
 
      //printf("\n CHECKING STATUS! \n");
-    // Recast task argument pointer to task’s data structure type
+    // Recast task argument pointer to taskâ€™s data structure type
     statusData*word=(statusData*)data;
     unsigned short*temp3=(unsigned short*)(*word).batteryStatePtr;
    
+    //Check if battery state is at 0, to prevent unwanted values being displayed   
+    if (*temp3 !=0 )	 
     --*temp3;//= *temp3 - 1;
     //(*word).batteryStatePtr = (unsigned char *)(*temp3);
 	return;
